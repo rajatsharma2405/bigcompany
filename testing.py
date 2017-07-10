407906962e76b1db2f42466425db652d1c6f81ca
rajat sharma <imrajat11@gmail.com>
2017-07-10 09:17:07 +0530

    commenting

--- testing.py
+++ testing.py
@@ -31,6 +31,7 @@ self_info()
 
 # Function for obtaining user-id from username
 insta_username = raw_input("what is the username")
+
 def get_user_id(insta_username):
     request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
@@ -155,3 +156,23 @@ def like_a_post(insta_username):
         print 'Your like was unsuccessful. Try again!'
 
 like_a_post(insta_username)
+
+
+
+# Function to comment on user's post
+def post_a_comment(insta_username):
+    media_id = get_post_id(insta_username)
+    comment_text = raw_input("Your comment: ")
+    payload = {"access_token": APP_ACCESS_TOKEN, "text": comment_text}
+    request_url = (BASE_URL + 'media/%s/comments') % (media_id)
+    print 'POST request url : %s' % (request_url)
+
+    make_comment = requests.post(request_url, payload).json()
+
+    if make_comment['meta']['code'] == 200:
+        print "Successfully added a new comment!"
+    else:
+        print "Unable to add comment. Try again!"
+
+post_a_comment(insta_username)
+
